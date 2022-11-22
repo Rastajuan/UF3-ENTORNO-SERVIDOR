a62c738ba394f362593f5c623377b30e01eda6a3
Juan Bello <36510813+Rastajuan@users.noreply.github.com>
2022-11-22 19:58:13 +0100

    JS validacion registro finalizado

--- js/registro.js
+++ js/registro.js
@@ -1,1 +1,39 @@
+document
+	.getElementById("botonRegistrar")
+	.addEventListener("click", validarRegistro);
 
+//Validacion del formulario de registro
+function validarRegistro() {
+	var usuario = document.getElementById("usuario").value;
+	var nombre = document.getElementById("nombre").value;
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+	var password2 = document.getElementById("password2").value;
+
+	if (usuario == "") {
+		alert("El campo usuario es obligatorio");
+		return false;
+	}
+	if (nombre == "") {
+		alert("El campo nombre es obligatorio");
+		return false;
+	}
+
+	if (email == "") {
+		alert("El campo email es obligatorio");
+		return false;
+	}
+	if (password == "") {
+		alert("El campo password es obligatorio");
+		return false;
+	}
+	if (password2 == "") {
+		alert("El campo password2 es obligatorio");
+		return false;
+	}
+	if (password != password2) {
+		alert("Las contraseñas no coinciden");
+		return false;
+	}
+	return true;
+}
