05e45f6d0ef2205a16ac1475681eaed29ca894b6
Juan Bello <36510813+Rastajuan@users.noreply.github.com>
2022-11-25 10:59:22 +0100

    Añadida clase usuario

---
+++ clases/usuario.php
@@ -0,0 +1,19 @@
+<?php
+class Usuario
+{
+
+    public $id;
+    public $nombre;
+    public $correo;
+    public $contraseña;
+
+
+
+    function __construct($id, $contraseña, $correo, $nombre)
+    {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->correo = $correo;
+        $this->contraseña = $contraseña;
+    }
+}
